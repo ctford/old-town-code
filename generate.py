d849d829d09a09ffa65d333f8ac1b2732f2384fe
Chris Ford <christophertford@gmail.com>
2019-11-25 15:44:39 +0100

    Use 0 and 1 to indicate key, and display linear and logistic predictions.

--- generate.py
+++ generate.py
@@ -1,24 +1,31 @@
 import csv
 import random
 
-samplesPerKey = 10000
 keys = {
   "C": 0,
+  "D": 2,
+  "E": 4,
+  "F": 5,
+  "G": 7,
+  "A": 9,
   "B": 11
 }
 
-def major(key, rank):
-  if rank > 6:
-    return major(key, rank - 7) + 12
+def in_major(m):
+  if m > 11:
+    return in_major(m - 12)
   else:
-    return keys[key] + [0, 2, 4, 5, 7, 9, 11][rank]
+    return 1 if m in keys.values() else 0
 
-ranks = random.choices(range(0, 56), k=samplesPerKey)
-notes = [[k, major(k, r)] for k in keys.keys() for r in ranks]
+def in_key(k, m):
+  return in_major(m - keys[k] + 12)
+
+midis = range(0, 128)
+notes = [[m, in_key("A", m), in_key("B", m), in_key("C", m), in_key("D", m)] for m in midis]
 
 with open('notes.csv', 'w') as csvFile:
     writer = csv.writer(csvFile)
-    writer.writerow(["Key", "Midi"])
+    writer.writerow(["Midi", "A", "B", "C", "D"])
     writer.writerows(notes)
 
 csvFile.close()
